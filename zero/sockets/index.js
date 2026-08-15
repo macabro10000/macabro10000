0290ab819dfc39243b63fbcab3e4f22c370de18f
macabro10000 <138259481+macabro10000@users.noreply.github.com>
2026-08-15 11:57:08 -0500

    Implement socket setup for player and territory connections
    Set up socket connections for players, territories, and maps.

--- zero/sockets/index.js
+++ zero/sockets/index.js
@@ -1,1 +1,48 @@
+const playerSocket = require("./playerSocket");
+const territorySocket = require("./territorySocket");
+const mapSocket = require("./mapSocket");
 
+module.exports = function setupSockets(
+  io,
+  players,
+  territories
+) {
+
+  io.on("connection", (socket) => {
+
+    console.log(
+      `🟢 Jugador conectado: ${socket.id}`
+    );
+
+    playerSocket(
+      socket,
+      io,
+      players,
+      territories
+    );
+
+    territorySocket(
+      socket,
+      io,
+      players,
+      territories
+    );
+
+    mapSocket(
+      socket,
+      io,
+      players,
+      territories
+    );
+
+    socket.on("disconnect", () => {
+
+      console.log(
+        `🔴 Jugador desconectado: ${socket.id}`
+      );
+
+    });
+
+  });
+
+};
